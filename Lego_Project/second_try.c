a1b9b4e3829bb1d2cc87195deba72d8429a7c128
Tony Hasson <63504193+tonyhasson@users.noreply.github.com>
2022-11-09 15:35:43 +0200

    Create second_try.c

---
+++ Lego_Project/second_try.c
@@ -0,0 +1,65 @@
+
+#pragma config(StandardModel, "EV3_REMBOT")
+#pragma config(Sensor, S3,     Colour,         sensorEV3_Color, modeEV3Color_Color)
+task main()
+{
+	int flag=0;
+	short currentColour;
+	while (true)
+	{
+		// Colours range from 0 to 7
+	  // None    = 0
+	  // Black   = 1
+	  // Blue    = 2
+	  // Green   = 3
+	  // Yellow  = 4
+	  // Red     = 5
+	  // White   = 6
+	  // Brown   = 7
+		currentColour = SensorValue[Colour];
+
+		switch(currentColour)
+		{
+			case 0:
+						setMotorSpeed(leftMotor, 0);		//Set the leftMotor (motor1) to half power (50)
+						setMotorSpeed(rightMotor, 0);  //Set the rightMotor (motor6) to half power (50)
+				    displayCenteredBigTextLine(4, "none");
+				    if(flag==0)
+				    {
+				    	flag=1;
+				    }
+				    else
+				    {
+				    	flag=0;
+				    }
+
+				    break;
+			case 1:
+						setMotorSpeed(leftMotor, 20);		//Set the leftMotor (motor1) to half power (50)
+						setMotorSpeed(rightMotor, 20);  //Set the rightMotor (motor6) to half power (50)
+				    displayCenteredBigTextLine(4, "black");
+				    break;
+
+			default:
+						if(flag==0)
+						{
+								setMotorSpeed(leftMotor, 20);		//Set the leftMotor (motor1) to half power (50)
+								setMotorSpeed(rightMotor, 5);  //Set the rightMotor (motor6) to half power (50)
+				    		displayCenteredBigTextLine(4, "unknown 0");
+				    		break;
+						}
+						else
+						{
+								setMotorSpeed(leftMotor, 5);		//Set the leftMotor (motor1) to half power (50)
+								setMotorSpeed(rightMotor, 20);  //Set the rightMotor (motor6) to half power (50)
+				    		displayCenteredBigTextLine(4, "unknown 1");
+				   		  break;
+						}
+
+
+		}
+
+
+		sleep(20);
+	}
+}
